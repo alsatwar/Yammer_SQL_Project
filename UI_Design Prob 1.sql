3b3ec1a56cca19b6f345469dc36edf87c7acfcd5
Alsatwar Sravan Kumar <50696588+alsatwar@users.noreply.github.com>
2021-01-08 14:40:07 +0530

    Delete UI_Design Prob 1.sql

--- UI_Design Prob 1.sql
+++
@@ -1,5 +0,0 @@
-SELECT device, COUNT(DISTINCT user_id) AS 'After 3rd Aug' FROM yammer_project.yammer_events
-WHERE event_type = 'engagement' AND
-      date(occurred_at) >= '2014-08-03'
-GROUP BY device
-ORDER BY 2 DESC;
